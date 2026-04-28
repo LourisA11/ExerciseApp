a67d733a707741ec14500728ad8179e158715e19
LourisA <147088420+Loura1928@users.noreply.github.com>
2026-04-27 21:11:42 -0400

    update index.ts to match db more

--- server/types/index.ts
+++ server/types/index.ts
@@ -1,79 +1,29 @@
-export type { DataEnvelope, DataListEnvelope } from "./dataEnvelope"
-
-export type ProductReview = {
-    rating: number
-    comment: string
-    date: string
-    reviewerName: string
-    reviewerEmail: string
+export type UsersRow = {
+  id: string
+  created_at: string
+  firstName: string
+  lastName: string
+  height: number
+  weight: number
+  age: number
+  email: string | null
+  role: "admin" | "moderator" | "user" | null
 }
 
-export type Product = {
-    id: number
-    title: string
-    description: string
-    category: string
-    price: number
-    tags: string[]
-    brand?: string
-    reviews: ProductReview[]
-    images: string[]
-    thumbnail: string
-}
-export const productKeys: (keyof Product)[] = [
-    "title",
-    "description",
-    "category",
-    "price",
-    "tags",
-    "brand",
-    "images",
-    "thumbnail",
-]
-
-export type CartItem = {
-    product: Product
-    quantity: number
+export type ExerciseBankRow = {
+  id: string
+  created_at: string
+  name: string
+  type: string
 }
 
-export type UserRole = "admin" | "moderator" | "user"
-
-export type UserAddress = {
-    address: string
-    city: string
-    state: string
-    stateCode: string
-    postalCode: string
-    country: string
+export type UserExerciseRow = {
+  id: number
+  created_at: string
+  user_id: string | null
+  exercise_id: string | null
+  weight_lb: number | null
+  reps: number | null
+  durations_min: number | null
+  distance: number | null
 }
-export const userAddressKeys: (keyof UserAddress)[] = [
-    "address",
-    "city",
-    "state",
-    "stateCode",
-    "postalCode",
-    "country",
-]
-
-export type User = {
-    id: number
-    firstName: string
-    lastName: string
-    gender: string
-    email: string
-    phone: string
-    birthDate: string
-    image: string
-    address: UserAddress
-    role: UserRole
-}
-export const userKeys: (keyof User)[] = [
-    "firstName",
-    "lastName",
-    "gender",
-    "email",
-    "phone",
-    "birthDate",
-    "image",
-    "role",
-]
